2362cd04ff86a29042c9842891a9be3ea487da0d
DESKTOP-QAGTK6B\user <test@test.com>
2024-06-17 18:50:29 +0900

    240617 insert, update 하기

--- src/main/java/com/example/lesson03/bo/ReviewBO.java
+++ src/main/java/com/example/lesson03/bo/ReviewBO.java
@@ -12,10 +12,32 @@ public class ReviewBO {
 	@Autowired
 	private ReviewMapper reviewMapper;
 	
+	// ex01
+	
 	// input: (int)id
 	// output: Review(단건) or null
 	public Review getReviewById(int id) {
 		return reviewMapper.selectReviewById(id);
 	}
 	
+	// ex02
+	
+	// input: Review
+	// output: 성공한 행의 개수(int)
+	public int addReview(Review review) {
+		return reviewMapper.insertReview(review);
+	}
+	
+	public int addReviewAsfield(int storeId, String menu,
+			 String userName, Double point, String review) {
+		return reviewMapper.insertReviewAsfield(storeId, menu, userName, point, review);
+	}
+	
+	// ex03
+	
+	// input: id, review
+	// output: 성공한 행의 개수(int)
+	public int updateReviewById(int id, String review) {
+		return reviewMapper.updateReviewById(id, review);
+	}
 }
--- src/main/java/com/example/lesson03/mapper/ReviewMapper.java
+++ src/main/java/com/example/lesson03/mapper/ReviewMapper.java
@@ -1,14 +1,30 @@
 package com.example.lesson03.mapper;
 
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
 import com.example.lesson03.domain.Review;
 
 @Mapper
 public interface ReviewMapper {
 	
+	// ex01
 	// input: (int)id
 	// output: Review or null
 	public Review selectReviewById(int id);
+		
+	// ex02
+	public int insertReview(Review review);
 	
+	public int insertReviewAsfield(
+			@Param("storeId")int storeId, 
+			@Param("menu")String menu,
+			@Param("userName")String userName, 
+			@Param("point")Double point, 
+			@Param("review")String review);
+	
+	// ex03
+	public int updateReviewById(
+			@Param("id") int id, 
+			@Param("review") String review);
 }
