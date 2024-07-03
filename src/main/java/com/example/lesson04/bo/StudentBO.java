c75051b0a9d8db621b698bfe6996147f5b74dcfc
DESKTOP-QAGTK6B\user <test@test.com>
2024-07-03 18:51:23 +0900

    240703 Lesson07  JPA Update, Delete, Select 예제

--- src/main/java/com/example/lesson04/bo/StudentBO.java
+++ src/main/java/com/example/lesson04/bo/StudentBO.java
@@ -1,6 +1,7 @@
 package com.example.lesson04.bo;
 
 import java.time.LocalDateTime;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -19,7 +20,7 @@ public class StudentBO {
 	@Autowired
 	private StudentRepository studentRepository;
 	
-	// lesson07 ex01
+	// lesson07 ex01_1 insert
 	// input: 파라미터 4개 (name, phoneNumber, email, dreamJob)
 	// output: StudentEntity
 	public StudentEntity addStudent(String name, String phoneNumber, String email, String dreamJob) {
@@ -33,6 +34,40 @@ public class StudentBO {
 				.build();
 		
 		return studentRepository.save(student);
+	}
+	
+	// lesson07 ex01_2 update
+	// input: id, dreamJob
+	// output: StudentEntity or null (리턴)
+	public StudentEntity updateStudentDreamJobById(int id, String dreamJob) {
+		// 기존 데이터를 조회 => StudentEntity
+		StudentEntity student = studentRepository.findById(id).orElse(null);
+		
+		// Entity의 데이터 값을 변경해놓는다.
+		if (student != null) {
+			student = student.toBuilder() // tobuilder는 기존 필드값은 유지하고 일부만 변경
+					.dreamJob(dreamJob)
+					.build(); // 꼭 객체에 다시 저장!!	
+			
+			// update
+			// save 요청
+			student = studentRepository.save(student);
+		}
+		return student;
+	}
+	
+	// lesson07 ex01_3 delete
+	// input: id
+	// output: X
+	public void deleteStudentById(int id) {
+		// 방법 1)
+//		StudentEntity student = studentRepository.findById(id).orElse(null);
+//		if (student != null) {
+//			studentRepository.delete(student);
+//		}
+		// 방법 2)
+		Optional<StudentEntity> studentOptional = studentRepository.findById(id);
+		studentOptional.ifPresent(s -> studentRepository.delete(s));
 	}
 	
 	// input: Student
--- src/main/java/com/example/lesson07/repository/StudentRepository.java
+++ src/main/java/com/example/lesson07/repository/StudentRepository.java
@@ -1,5 +1,7 @@
 package com.example.lesson07.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.lesson07.entity.StudentEntity;
@@ -7,6 +9,19 @@ import com.example.lesson07.entity.StudentEntity;
 public interface StudentRepository extends JpaRepository<StudentEntity, Integer> {
 	
 	// String Data JPA vs 순수 JPA
-	// public StudentEntity save(StudentEntity student)
+	// public StudentEntity save(StudentEntity student);	
+	// public StudentEntity find(조회)ById(int id);
+	// public void delete(StudentEntity student);
+	// public List<StudentEntity> findAll();
+	
+	// ex02/select1 - JPQL 문법
+	public List<StudentEntity> findAllByOrderByIdDesc();
+	public List<StudentEntity> findTop3ByOrderByIdDesc();
+	public List<StudentEntity> findByName(String name111);
+	public List<StudentEntity> findBynameIn(List<String> names);
+	public List<StudentEntity> findByNameAndDreamJob(String name, String dreamJob);
+	public List<StudentEntity> findByEmailContains(String email);
+	public List<StudentEntity> findByNameStartingWith(String name);
+	public List<StudentEntity> findByIdBetween(int startId, int endId);
 	
 }
